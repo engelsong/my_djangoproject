fa8e87bb9915bda30a0a5198f896c98a832349fe
engelsong <engelsong@126.com>
2022-03-03 16:48:00 +0800

    modify view.py in pages app

--- my_root/pages/views.py
+++ my_root/pages/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<h1>The HomePage</h1>")
+    # return HttpResponse("<h1>The HomePage</h1>")
+    return render(request, 'page/page.html')
 
 # Create your views here.
